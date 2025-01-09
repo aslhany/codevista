74c6034034ad6c4298a1fc10497701323c996c7d
aslhany <aslihanyigit28@gmail.com>
2025-01-09 10:36:37 +0300

    Update login.php

--- login.php
+++ login.php
@@ -50,7 +50,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="anasayfa.html">Home</a>
+                  <a class="nav-link active" aria-current="page" href="index.html">Home</a>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link" href="hakkımızda.html">About Us</a>
